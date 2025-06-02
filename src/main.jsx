e66affd5585e0fbdcb250afebd87569696d405fa
Yordi984 <135142211+Yordi984@users.noreply.github.com>
2025-06-02 18:25:40 -0500

    funcionando elimine lo de rafa xq estaba mal y causaba errores

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import LoginEstudiante from "./pages/LoginEstudiante/LoginEstudiante";
 import CrearTrabajos from "./pages/Trabajos/CrearTrabajo";
-import Ver from "./pages/Trabajos/Ver";
 import LoginEmpresa from "./pages/CrearEmpresa/CrearEmpresa";
 import Elegir from "./pages/Elegir/Elegir";
-
-import App from "./app";
 import Login from "./pages/Login/login";
+import Home from "./pages/Home/Home";
 import Demo from "./pages/Demo/Dashboard";
 
 const root = document.getElementById("root");
@@ -16,24 +14,13 @@ const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Home />} />
       <Route path="/Elegir" element={<Elegir />} />
-
       <Route path="/CrearEstudiante" element={<Login />} />
       <Route path="/LoginEstudiante" element={<LoginEstudiante />} />
-
       <Route path="/Demo" element={<Demo />} />
-<<<<<<< HEAD
-
       <Route path="/CrearTrabajos" element={<CrearTrabajos />} />
       <Route path="CrearEmpresa" element={<LoginEmpresa />} />
-
-      <Route path="/Trabajos/Ver" element={<Ver />} />
-=======
-      <Route path="/PerfilReclutador" element={<PerfilReclutador />} />
-      <Route path="/PerfilEstudiante" element={<PerfilEstudiante />} />
-      <Route path="/Registro" element={<Registro />} />
->>>>>>> 849cd1fc6ec5a84fe03fe4aaad88c5b69b6c698c
     </Routes>
   </BrowserRouter>
 );
